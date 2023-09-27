165bce38ea29210cdc45a6fd82e2cc0250de2cca
Ashley65 <nchinembiri@outlook.com>
2023-09-28 00:59:05 +0100

    start

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by NIgel work on 28/09/2023.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by NIgel work on 28/09/2023.
+//
+
+#ifndef PASSWORD_MANAGER_MAIN_H
+#define PASSWORD_MANAGER_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PASSWORD_MANAGER_MAIN_H
